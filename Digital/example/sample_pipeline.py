a58ca21cfffe717ab2273a4565e0dbde0ba2a38d
Gculb <grantc24@vt.edu>
2026-02-26 22:46:29 -0500

    added sample files and added main

--- Digital/example/sample_pipeline.py
+++ Digital/example/sample_pipeline.py
@@ -3,12 +3,15 @@
 from core.signal_processor import SignalProcessor
 from io.signal_generator import generate_noisy_signal
 
+def main():
+    SAMPLE_RATE = 1000
+    DURATION = 2
 
-SAMPLE_RATE = 1000
-DURATION = 2
+    signal = generate_noisy_signal(freq=50, duration=DURATION, sample_rate=SAMPLE_RATE)
+    sp = SignalProcessor(signal, SAMPLE_RATE)
+    sp.apply_lowpass(100)
+    sp.normalize()
+    sp.plot_time_domain() 
 
-signal = generate_noisy_signal(freq=50, duration=DURATION, sample_rate=SAMPLE_RATE)
-sp = SignalProcessor(signal, SAMPLE_RATE)
-sp.apply_lowpass(100)
-sp.normalize()
-sp.plot_time_domain() 
+if __name__ == "__main__":
+    main() 
